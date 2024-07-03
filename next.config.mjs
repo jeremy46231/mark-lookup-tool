29444d02f91f436016a65e98235e3216a8575762
Jeremy <jeremyrwoolley+git@gmail.com>
2024-07-03 00:24:36 +0000

    Begin work on a mobile layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: ['localhost:3000', 'urban-space-zebra-wrvqwv6gpp5w29v6j-3000.app.github.dev'],
+        },
+    },
+};
 
 export default nextConfig;
